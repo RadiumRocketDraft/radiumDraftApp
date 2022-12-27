fb988f743c02aa2349ad32a9ab7f6d03b7f43532
Ignacio Aristo <ignacioaristo@gmail.com>
2022-12-27 14:02:12 -0300

    RRD-5: implement login form

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NativeBaseProvider} from 'native-base';
+
 import Login from './src/screens/login';
 import Home from './src/screens/home';
 
@@ -9,10 +11,12 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
+      <NativeBaseProvider>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NativeBaseProvider>
     </NavigationContainer>
   );
 };
--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,11 +1,80 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Stack, Input, Button} from 'native-base';
+import {useForm, Controller} from 'react-hook-form';
+
+interface ILogin {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
+  const {
+    control,
+    handleSubmit,
+    // formState: {errors},
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = (data: ILogin) => {
+    console.log(data);
+  };
   return (
-    <View>
-      <Text>LOGIN SCREEN</Text>
-    </View>
+    <Stack
+      space={4}
+      w="100%"
+      h="100%"
+      alignItems="center"
+      justifyContent="center">
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({field: {onChange, value}}) => (
+          <Input
+            onChangeText={onChange}
+            value={value}
+            w={{
+              base: '75%',
+              md: '25%',
+            }}
+            mb={15}
+            placeholder="E-mail"
+            autoCapitalize="none"
+          />
+        )}
+        name="email"
+      />
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({field: {onChange, value}}) => (
+          <Input
+            onChangeText={onChange}
+            value={value}
+            type="password"
+            placeholder="Password"
+            autoCapitalize="none"
+            w={{
+              base: '75%',
+              md: '25%',
+            }}
+            mb={15}
+          />
+        )}
+        name="password"
+      />
+
+      <Button onPress={handleSubmit(onSubmit)} w={'50%'}>
+        Login
+      </Button>
+    </Stack>
   );
 };
 
