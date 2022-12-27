9dc704477e18ee6ffba82608d98d0632913ab0a0
Ignacio Aristo <ignacioaristo@gmail.com>
2022-12-27 16:26:24 -0300

    RRD-7: create bottom tab navigator

--- App.tsx
+++ App.tsx
@@ -2,19 +2,33 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NativeBaseProvider} from 'native-base';
-
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import Login from './src/screens/login';
 import Home from './src/screens/home';
+import Profile from './src/screens/profile';
+import History from './src/screens/history';
 
 const Stack = createNativeStackNavigator();
+
+const Tab = createMaterialBottomTabNavigator();
+
+const BottomNavigation = () => {
+  return (
+    <Tab.Navigator activeColor="#f0edf6" inactiveColor="#3e2465">
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="History" component={History} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+};
 
 const App = () => {
   return (
     <NavigationContainer>
       <NativeBaseProvider>
         <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Home" component={BottomNavigation} />
           <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Home" component={Home} />
         </Stack.Navigator>
       </NativeBaseProvider>
     </NavigationContainer>
---
+++ src/screens/profile/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>Profile SCREEN</Text>
+    </View>
+  );
+};
+
+export default Profile;
