12e958d57b676216e999795d60bf313f6ce6189a
Martin Raveglia <martin.raveglia@radiumrocket.com>
2023-01-03 15:03:28 -0300

    RRD-34: Update shared button component

--- src/components/shared/button/index.tsx
+++ src/components/shared/button/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import {Button as ButtonNativeBase} from 'native-base';
+import {GestureResponderEvent, StyleProp, ViewStyle} from 'react-native';
 
-const Button = ({text, handleSubmit}: any) => {
+interface Props {
+  text: string;
+  handleSubmit?: (event: GestureResponderEvent) => void;
+  style?: StyleProp<ViewStyle>;
+  isLoading?: boolean;
+}
+
+const Button = ({text, handleSubmit, style, isLoading}: Props) => {
   return (
-    <ButtonNativeBase onPress={handleSubmit} w={'50%'}>
+    <ButtonNativeBase
+      onPress={handleSubmit}
+      w={'50%'}
+      style={style}
+      isLoading={isLoading}>
       {text}
     </ButtonNativeBase>
   );
