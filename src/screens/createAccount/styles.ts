301d22578b1c97640a8a0bb62981095b5556ab2d
julianv97 <julian.vicente@radiumrocket.com>
2023-01-04 15:42:46 -0300

    RRD-43: delete styles from create account screen

--- src/screens/createAccount/styles.ts
+++
@@ -1,9 +0,0 @@
-import {StyleSheet} from 'react-native';
-
-export default StyleSheet.create({
-  sliderContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-});
