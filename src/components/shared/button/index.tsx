94612ba4867eecb4ecc4a6d45c959e0e14f380c0
Ignacio Aristo <ignacioaristo@gmail.com>
2023-01-05 10:48:45 -0300

    RRD-31: add styles to button

--- src/components/shared/button/index.tsx
+++ src/components/shared/button/index.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import {Button as ButtonNativeBase} from 'native-base';
 import {GestureResponderEvent, StyleProp, ViewStyle} from 'react-native';
+import styles from './styles';
 
 interface Props {
   text: string;
   handleSubmit?: (event: GestureResponderEvent) => void;
-  style?: StyleProp<ViewStyle>;
+  customStyle?: StyleProp<ViewStyle>;
   isLoading?: boolean;
   isDisabled?: boolean;
 }
 
-const Button = ({text, handleSubmit, style, isLoading, isDisabled}: Props) => {
+const Button = ({
+  text,
+  handleSubmit,
+  customStyle,
+  isLoading,
+  isDisabled,
+}: Props) => {
   return (
     <ButtonNativeBase
       onPress={handleSubmit}
       w={'50%'}
-      style={style}
+      style={[customStyle, styles.bottomButton]}
       isLoading={isLoading}
-      disabled={isDisabled}>
+      isDisabled={isDisabled}>
       {text}
     </ButtonNativeBase>
   );
