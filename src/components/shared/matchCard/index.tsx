734ff308cfb7a5d6ae0660e03ef4e8782b6d0e5a
Ignacio Aristo <ignacioaristo@gmail.com>
2023-01-10 16:27:27 -0300

    RRD-57: add real data to the cards

--- src/components/shared/matchCard/index.tsx
+++ src/components/shared/matchCard/index.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import styles from './styles';
+import format from 'date-fns/format';
 
 const MatchCard = ({match}: any) => {
+  const scoreTeamA = match?.item.result?.teamA;
+  const scoreTeamB = match?.item.result?.teamB;
+  const matchDate = format(new Date(match?.item.date), 'MM/dd/yyyy'); // Cuando se conecte la creación de partido modificar esto con lo que viene
+  const field = match?.item.field; // Cuando se conecte la creación de partido modificar esto con lo que viene
+
   return (
     <View style={styles.container}>
       <View style={styles.cardContainer}>
         <View style={styles.columnData}>
           <Text>Result</Text>
-          <Text>5-4</Text>
+          <Text>
+            {scoreTeamA} - {scoreTeamB}
+          </Text>
         </View>
         <View style={styles.columnData}>
           <Text>Date</Text>
-          <Text>02/03/2023</Text>
+          <Text>{matchDate}</Text>
         </View>
         <View style={styles.columnData}>
           <Text>Field</Text>
-          <Text>La previa</Text>
+          <Text>{field}</Text>
         </View>
       </View>
       <View style={styles.trapezoidLeft} />
